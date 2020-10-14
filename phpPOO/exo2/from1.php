0716764a9fa70b31ae460f1431383ce5e115a913
Nathan Wurtz <nathan.wurtz01@gmail.com>
2020-10-14 14:37:22 +0200

    2

---
+++ phpPOO/exo2/from1.php
@@ -0,0 +1,11 @@
+<?php
+class Form1 
+{
+    private $content;
+    function __construct($method)
+    {
+        $content = "<form action=processForm.php method=\"$method\">";
+    }
+    function field 
+}
+?>
